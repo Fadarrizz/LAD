01ee9e42b44473cd0eaf750b85b8709a6dd3479b
Leyla <leyla_banchaewa@hotmail.com>
2017-11-13 15:24:28 +0100

    added classes.py

---
+++ amstelhaege/Classes.py
@@ -0,0 +1,44 @@
+class House:
+    """Class definition for a detached house."""
+    width = 8
+    length = 8
+    meters = 2
+    price = 285000
+    percentage = 0.03
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.free_space =
+        self.value = price + price * (percentage * free_space)
+
+
+class Bungalow:
+    """Class definition for a bungalow."""
+    width = 10
+    length = 7.5
+    meters = 3
+    price = 399000
+    percentage = 0.04
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Maison:
+    """Class definition for a maison."""
+    width = 11
+    length = 10.5
+    meters = 6
+    price = 610000
+    percentage = 0.06
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Waterbody:
+    """Class definition for a waterbody."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
