0d0917496393bcc5c77ceab5b0e6e74460639699
Fadarrizz <auk.geerts@gmail.com>
2017-11-13 15:17:15 +0100

    MatPlotLib grid
    Created a grid with MatPlotLib

--- amstelhaege/grid.py
+++ amstelhaege/grid.py
@@ -1,5 +1,29 @@
-grid[][]
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
 
-for y in grid:
-    for x in grid:
-        print(".")
+eengezinswoning = [
+    (0., 0.),   # left, bottom
+    (0., 8.),  # left, top
+    (8., 8.), # right, top
+    (8., 0.),  # right, bottom
+    (0., 0.),   # ignored
+]
+
+codes = [
+    Path.MOVETO,
+    Path.LINETO,
+    Path.LINETO,
+    Path.LINETO,
+    Path.CLOSEPOLY,
+]
+
+path = Path(eengezinswoning, codes)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+patch = patches.PathPatch(path, facecolor='orange', lw=2)
+ax.add_patch(patch)
+ax.set_xlim(0, 160)
+ax.set_ylim(0, 180)
+plt.show()
