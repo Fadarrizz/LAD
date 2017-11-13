0929799b8bcefd320e3b4e8f0aba7d7b50485f41
Fadarrizz <auk.geerts@gmail.com>
2017-11-13 16:50:32 +0100

    Grid update

--- amstelhaege/grid.py
+++ amstelhaege/grid.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
+import matplotlib.ticker as plticker
 
 eengezinswoning = [
-    (0., 0.),   # left, bottom
-    (0., 8.),  # left, top
-    (8., 8.), # right, top
-    (8., 0.),  # right, bottom
+    (0.856, 0.),   # left, bottom
+    (0.856, 8.),  # left, top
+    (8.856, 8.), # right, top
+    (8.856, 0.),  # right, bottom
     (0., 0.),   # ignored
 ]
 
@@ -20,10 +21,24 @@ codes = [
 
 path = Path(eengezinswoning, codes)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+fig,ax=plt.subplots()
+
+# Fixed spacing
+plt.axis('scaled')
+
+# Spacing between each line
+intervals = 10
+
+loc = plticker.MultipleLocator(base=intervals)
+ax.xaxis.set_major_locator(loc)
+ax.yaxis.set_major_locator(loc)
+
 patch = patches.PathPatch(path, facecolor='orange', lw=2)
 ax.add_patch(patch)
+
+ax.grid(which='major', axis='both', linestyle='-')
+
+# ax.grid(color='gray', linestyle='dashed')
 ax.set_xlim(0, 160)
 ax.set_ylim(0, 180)
 plt.show()
