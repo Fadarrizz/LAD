b442a6040adef845032699f59835e08de24fa0e5
Auke Geerts <auk.geerts@gmail.com>
2017-12-13 21:58:57 +0100

    Attempt on move function + added some functions

--- amstelhaege/test/interference.py
+++
@@ -1,8 +0,0 @@
-def interference(x, x2, y, y2, xMIN, xMAX, yMIN, yMAX, bType):
-    if Overlap(x, x2, y, y2, xMIN, xMAX, yMIN, yMAX) or \
-       CheckFreespaceOverlap(bType, x, y) or \
-       WaterOverlap(x, y, x2, y2):
-       return True:
-    return False
-
-def set():
