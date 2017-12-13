92845d79534fbc75a9c612da8cbc1599febb8d6a
Auke Geerts <auk.geerts@gmail.com>
2017-12-13 21:58:57 +0100

    Attempt on move function + added some functions

---
+++ amstelhaege/algorithms/functions.py
@@ -0,0 +1,31 @@
+def collision(thisBuilding, x, x2, y, y2, bType):
+    """Checks if there is any collision with the chosen coordinates"""
+    for neighbour in buildingsPlaced:
+        yMIN = neighbour.y
+        xMIN = neighbour.x
+        yMAX = yMIN + neighbour.length
+        xMAX = xMIN + neighbour.width
+
+        # skip the indicated house
+        if (thisBuilding.name == neighbour.name):
+            pass
+        if Overlap(x, x2, y, y2, xMIN, xMAX, yMIN, yMAX) or \
+           CheckFreespaceOverlap(bType, x, y) or \
+           WaterOverlap(x, y, x2, y2):
+           return True
+    return False
+
+def GenerateCoordinates(bType, xBorder, yBorder):
+    x = randint(0, xBorder)
+    x2 = x + bType.width
+    y = randint(0, yBorder)
+    y2 = y + bType.length
+    return x, x2, y, y2
+
+def MoveAStep(side, coord, step):
+    if side == 'left' or 'down':
+        coord -= step
+    if side == 'right' or 'up':
+        coord += step
+    return coord
+    
--- amstelhaege/algorithms/move.py
+++ amstelhaege/algorithms/move.py
@@ -1,24 +1,47 @@
 from classes import *
 
-# pak huis
-# kies richting
-# zet stap
-# onthou score in array, undo stap
-# herhaal voor alle richtingen
-# kijk welke het beste is - TotalScore
-# zet beste stap
-# herhaal tot niet meer mogelijk
-# pak volgend huis
+def Move():
+    # pak huis
+    # kies richting
+    # zet stap
+    # onthou score in array, undo stap
+    # herhaal voor alle richtingen
+    # kijk welke het beste is - TotalScore
+    # zet beste stap
+    # herhaal tot niet meer mogelijk
+    # pak volgend huis
 
-# instantiate list
-buildingsPlaced = Building.buildingsPlaced
+    # instantiate list
+    buildingsPlaced = Building.buildingsPlaced
 
-# define step size
-step = 0.5
+    # define step size
+    step = 0.5
 
-for thisHouse in buildingsPlaced:
+    # total score
+    totalScore = TotalScore()
 
-    # step to right
-    thisHouse.x += step
+    # array for checking new total scores
+    newScores = []
 
-    if interference(x, x2, y, y2, xMIN, xMAX, yMIN, yMAX, thisHouse.bType)
+    for thisBuilding in buildingsPlaced:
+
+        x = thisBuilding.x
+        y = thisBuilding.x
+        x2 = x + thisBuilding.width
+        y2 = y + thisBuilding.length
+        bType = thisBuilding.house_type
+
+        sides = [('left', x), ('right', x), ('up', y), ('down', y)]
+        for side in sides:
+            side[1] = MoveAStep(side[0], side[1], step)
+            if collision(thisBuilding, x, x2, y, y2, bType):
+                # undo step (NOT DONE)
+                pass
+            newScores.append(TotalScore())
+            # undo step
+
+        # TODO:
+        # see which score is the best
+        # if non: go to next building
+        # otherwise, make that step
+        # go to next building
