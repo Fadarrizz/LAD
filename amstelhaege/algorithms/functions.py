ad61c29b8ebcbad50fad158707206c520a7277c2
Auke Geerts <auk.geerts@gmail.com>
2017-12-14 17:33:41 +0100

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master'"
    This reverts commit 175ee326d0fee954395bf2df23285894b7283a71.

--- amstelhaege/algorithms/functions.py
+++ amstelhaege/algorithms/functions.py
@@ -3,33 +3,6 @@
 
 
 
-    """Checks if there is any collision with the chosen coordinates"""
-    for neighbour in buildingsPlaced:
-        yMIN = neighbour.y
-        xMIN = neighbour.x
-        yMAX = yMIN + neighbour.length
-        xMAX = xMIN + neighbour.width
-
-        # skip the indicated house
-        if (thisBuilding.name == neighbour.name):
-            continue
-        if Overlap(x, x2, y, y2, xMIN, xMAX, yMIN, yMAX) or \
-           CheckFreespaceOverlap(bType, x, y) or \
-           WaterOverlap(x, y, x2, y2):
-           return True
-    return False
-
-def GenerateCoordinates(bType):
-    """Generates random coordinates"""
-    xBorder = int(cs_Grid.xMAX - bType.width)
-    yBorder = int(cs_Grid.yMAX - bType.length)
-
-    x = randint(0, xBorder)
-    x2 = x + bType.width
-    y = randint(0, yBorder)
-    y2 = y + bType.length
-    return x, x2, y, y2
->>>>>>> master
 
 def MoveAStep(side, coord, step):
     """Changes coordinate to the desired direction"""
@@ -67,5 +40,3 @@ def GetScoreSingleBuilding(totalScore, minusScore):
     """Calculates score of a single building"""
     bScore = totalScore - minusScore
     return bScore
-<<<<<<< HEAD
->>>>>>> master
--- amstelhaege/main.py
+++ amstelhaege/main.py
@@ -7,6 +7,9 @@
 # visualized in a figure on a grid, which also shows a total score of the
 # calculated total value of all the houses.
 
+# import matplotlib.pyplot as plt
+# import matplotlib.patches as patches
+# import matplotlib.ticker as plticker
 from algorithms.randomfunction import *
 import classes
 from helpers import *
